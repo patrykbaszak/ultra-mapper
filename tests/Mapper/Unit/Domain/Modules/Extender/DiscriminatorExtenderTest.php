9f4f841ed0cc4e5fbf98c8e3e15933220488f6ee
patrykbaszak <patryk.baszak@gmail.com>
2024-07-21 17:45:19 +0200

    feat: Added support for Discriminator attribute

---
+++ tests/Mapper/Unit/Domain/Modules/Extender/DiscriminatorExtenderTest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PBaszak\UltraMapper\Tests\Mapper\Unit\Domain\Modules\Checker;
+
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\TestCase;
+
+#[Group('unit')]
+class DiscriminatorExtenderTest extends TestCase
+{
+}
